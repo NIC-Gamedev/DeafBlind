ee200c5e3caa5969ec225a8c119af8faaeb769b4
ICEY_YOU <122779819+DeStiNoVich21@users.noreply.github.com>
2024-11-05 22:35:08 +0500

    Echolocation shader added

--- Assets/Main/Scripts/HealthComponent.cs
+++ Assets/Main/Scripts/HealthComponent.cs
@@ -12,7 +12,7 @@ public class ObjectHealth : MonoBehaviour
         currentHealth = maxHealth; 
     }
 
-    public void TakeHealth(float dmg)
+    public void GetDamage(float dmg)
     {
         currentHealth -= dmg;
     }
