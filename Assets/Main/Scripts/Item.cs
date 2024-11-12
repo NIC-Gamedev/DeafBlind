76653606a84477d44768f651face6384510ae09e
ICEY_YOU <122779819+DeStiNoVich21@users.noreply.github.com>
2024-11-12 22:12:45 +0500

    Abstract Item Scriptable object

--- Assets/Main/Scripts/Item.cs
+++ Assets/Main/Scripts/Item.cs
@@ -1,18 +1,29 @@
- using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : ScriptableObject
+public enum ItemType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Weapon,
+    Armor,
+    Potion,
+    QuestItem,
+    Miscellaneous
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public abstract class ItemData : ScriptableObject
+{
+    //≈сли хотем четко создать какой то обьект используем его, но или если чего то нехватает то просто его наследуем и добавл€ем туда что то нужное 
+    //[CreateAssetMenu(fileName = "New Potion", menuName = "Inventory/Potion")]
+
+    [Header("ќсновные данные предмета")]
+    public string itemName;            
+    public Sprite itemIcon;            
+    public ItemType itemType;          
+    [TextArea] public string description; 
+
+    [Header("ƒополнительные параметры")]
+    public int maxStack = 1;           
+    public bool isQuestItem;           
+
+   
+    public abstract void UseItem(GameObject user);
 }
