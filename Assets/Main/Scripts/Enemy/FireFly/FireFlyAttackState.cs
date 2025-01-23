fdd902b52907420cac06bf72af3c9a5187d5cac8
Rin <playmurka86@gmail.com>
2025-01-23 22:05:54 +0500

    Bit of FireFlyAi

---
+++ Assets/Main/Scripts/Enemy/FireFly/FireFlyAttackState.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlyAttackState : MonoBehaviour,IAIState
+{
+    private EnemyPerception _enemyPerception;
+    private EnemyPerception enemyPerception
+    {
+        get
+        {
+            if (_enemyPerception == null)
+            {
+                _enemyPerception = GetComponent<EnemyPerception>();
+            }
+            return _enemyPerception;
+        }
+    }
+    private EnemyMovement _enemyMovement;
+    private EnemyMovement enemyMovement
+    {
+        get
+        {
+            if (_enemyMovement == null)
+            {
+                _enemyMovement = GetComponent<EnemyMovement>();
+            }
+            return _enemyMovement;
+        }
+    }
+    private GiantAttack _AttackState;
+    private GiantAttack attackState
+    {
+        get
+        {
+            if (_AttackState == null)
+            {
+                _AttackState = GetComponent<GiantAttack>();
+            }
+            return _AttackState;
+        }
+    }
+
+
+    StateController controller;
+    public void EnterState(StateController owner)
+    {
+        controller = owner;
+        attackState.Attack();
+    }
+
+    public void ExitState()
+    {
+    }
+
+    public string GetStateName()
+    {
+        return "Attack";
+    }
+
+    public void UpdateState()
+    {
+        if (attackState.attackProcess == null)
+        {
+            controller.SetState<FireFlyChaseState>();
+        }
+    }
+}
