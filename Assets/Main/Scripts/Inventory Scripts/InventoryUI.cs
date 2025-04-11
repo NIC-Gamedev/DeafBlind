7eac415dd4b1fefcb5ee911df2bc6ad30e31ef08
ICEY_YOU <122779819+DeStiNoVich21@users.noreply.github.com>
2025-04-11 18:44:20 +0500

    fix/wakatime delete

---
+++ Assets/Main/Scripts/Inventory Scripts/InventoryUI.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public TextMesh item1, item2, item3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
